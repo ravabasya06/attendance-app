981e642de49e6f17d1343812f116822cfdc8c768
ravabasya06 <ravabasya06@gmail.com>
2024-09-20 16:08:12 +0700

    added more column to users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->bigInteger('tlp');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('posisi');
-            $table->string('peran');
+            $table->string('posisi'); // Developer
+            $table->string('peran'); // User / Admin
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
